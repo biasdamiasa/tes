f8d60becb64f5b10a94a9fdb5e25719289e57403
biasdamiasa <biasd30@gmail.com>
2020-08-08 11:39:02 +0700

    second

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tes
                 </div>
 
                 <div class="links">
